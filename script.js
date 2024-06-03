9b94c0d36318cb57ae533954647e76cad2c11e11
Sami Ullah <samiullahiqbal5@gmail.com>
2024-06-03 18:21:05 +0500

    update

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 const headerDiv = document.querySelector(".links");
 const selectMobile = document.querySelector(".mobile");
+const list = document.getElementById("list");
+let arr = [];
+console.log(list);
 let isTrue = true;
 selectMobile.onclick = () => {
   if (isTrue == true) {
@@ -16,9 +19,12 @@ selectMobile.onclick = () => {
 function getProduct() {
   fetch("http://localhost:3000/product").then((res) =>
     res.json().then((array) => {
-      // console.log(array);
+      // console.log(array.length);
+      list.innerText = `Total Products:${array.length} `;
       let getProductHtml = "";
       for (const data of array) {
+        arr = array;
+        console.log(arr);
         console.log(data);
         getProductHtml += `
         <div class="card-style">
@@ -31,7 +37,7 @@ function getProduct() {
         <p>${data.price}</p>
         <div class="style-btn">
         <button id="delete" value="${data.id}">Delete</button>
-        <button>Edit</button>
+        <button id= "edit" value="${data.id}">Edit</button>
         </div>
             </div>
         `;
@@ -52,7 +58,7 @@ function addProduct() {
   const price = document.getElementById("price");
   const submit = document.getElementById("submit");
   const showErr = document.getElementById("show-error");
-  console.log(image, title, description, price, submit, showErr, productList);
+  console.log(image, title, description, price, submit, showErr);
 
   submit.onclick = () => {
     if (image.value == "") {
@@ -77,10 +83,16 @@ function addProduct() {
       fetch("http://localhost:3000/product", options).then((res) =>
         res.json().then(getProduct())
       );
+      showErr.innerText = "Added Successfully";
+      showErr.style.color = "green";
     }
     setTimeout(() => {
       showErr.innerText = "";
       showErr.style.color = "red";
+      image.value = "";
+      title.value = "";
+      description.value = "";
+      price.value = "";
     }, 1700);
   };
 }
@@ -106,3 +118,56 @@ function deleteProduct() {
 }
 deleteProduct();
 // DeleteProduct End
+
+// UpdateProduct Start
+function updateProduct() {
+  const image = document.getElementById("image-edit");
+  const title = document.getElementById("title-edit");
+  const description = document.getElementById("description-edit");
+  const price = document.getElementById("price-edit");
+  const edit = document.getElementById("edit");
+  const update = document.getElementById("update");
+  console.log(image, title, description, price, edit, update);
+  let id = "";
+  document.onclick = (e) => {
+    if (e.target.id == "edit") {
+      id = e.target.value;
+
+      const checkRecord = arr.find((item) => item.id == id);
+      console.log(checkRecord);
+
+      image.value = checkRecord.image;
+      title.value = checkRecord.title;
+      description.value = checkRecord.description;
+      price.value = checkRecord.price;
+    }
+  };
+  update.onclick = () => {
+    const productUpdated = document.getElementById("product-update");
+    // console.log(productUpdated);
+    const body = {
+      image: image.value,
+      description: description.value,
+      title: title.value,
+      price: price.value,
+    };
+    const options = {
+      method: "PUT",
+      body: JSON.stringify(body),
+    };
+    fetch(`http://localhost:3000/product/${id}`, options).then((res) =>
+      res.json().then(getProduct())
+    );
+    productUpdated.innerText = "Updated Successfully";
+
+    setTimeout(() => {
+      image.value = "";
+      title.value = "";
+      description.value = "";
+      price.value = "";
+      productUpdated.innerText = "";
+    }, 1700);
+  };
+}
+updateProduct();
+// UpdateProduct End
